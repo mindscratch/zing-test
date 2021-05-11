5db285572a5709e9645d09ba799b10013fdf81e1
Craig Wickesser <codecraig@gmail.com>
2021-05-11 15:59:21 -0400

    angular 11 project trying to use ZingChart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zing-test';
+  config: zingchart.graphset = {
+    type: "bar",
+    title: {
+      text: "Hello ZingChart Angular Edition!"
+    },
+    series: [
+      {
+        values: [4, 5, 3, 4, 5, 3, 5, 4, 11]
+      }
+    ]
+  };
 }
